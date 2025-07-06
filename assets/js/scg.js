3cb7d5dac03654b3aff2c8a251b62c094bd397a7
Dave DeLong <github@davedelong.com>
2025-07-05 22:32:50 -0600

    Update scg.js

--- assets/js/scg.js
+++ assets/js/scg.js
@@ -1,3 +1,8 @@
+const extSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+  <path fill-rule="evenodd" d="M15.75 2.25H21a.75.75 0 0 1 .75.75v5.25a.75.75 0 0 1-1.5 0V4.81L8.03 17.03a.75.75 0 0 1-1.06-1.06L19.19 3.75h-3.44a.75.75 0 0 1 0-1.5Zm-10.5 4.5a1.5 1.5 0 0 0-1.5 1.5v10.5a1.5 1.5 0 0 0 1.5 1.5h10.5a1.5 1.5 0 0 0 1.5-1.5V10.5a.75.75 0 0 1 1.5 0v8.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V8.25a3 3 0 0 1 3-3h8.25a.75.75 0 0 1 0 1.5H5.25Z" clip-rule="evenodd" />
+</svg>
+`;
+
 function linkifyAnchor(element) {
   const href = element.getAttribute("href");
   if (href.startsWith("/")) { return; }
@@ -5,6 +10,11 @@ function linkifyAnchor(element) {
   if (href.startsWith("https://www.sandersoncollectorsguild.com/")) { return; }
 
   href.setAttribute("target", "_blank");
+
+  var svgDiv = document.createElement('div');
+  svgDiv.innerHTML = extSVG;
+  const svgNode = svgDiv.firstElementChild;
+  element.appendChild(svgNode);
 }
 
 function linkify() {
@@ -13,5 +23,3 @@ function linkify() {
     linkifyAnchor(aTags[i]);
   }
 }
-
-linkify();
